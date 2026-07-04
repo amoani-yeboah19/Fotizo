fe661943eb4740ff21d00631211d3f93b5750c87
Bright <bamoani-yeboah@st.ug.edu.gh>
2026-07-04 17:43:05 +0000

    updated logo

--- artifacts/fotizo/src/app/main.tsx
+++ artifacts/fotizo/src/app/main.tsx
@@ -3,3 +3,10 @@ import App from "./App";
 import "@/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Dismiss the boot splash (declared in index.html) once the app has mounted.
+const splash = document.getElementById("app-splash");
+if (splash) {
+  requestAnimationFrame(() => splash.classList.add("is-hidden"));
+  setTimeout(() => splash.remove(), 700);
+}
